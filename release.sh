f48e39b854cb9cd746ad2f286cdb6193dd7d9b3e
Javi Perez-Griffo <javier@besol.es>
2017-07-26 15:39:43 +0200

    Release 0.4.3

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Please export GITHUB_TOKEN variable"
+    exit 1
+fi
+go get github.com/goreleaser/goreleaser
 git fetch --tags
 pwd=`git rev-parse --show-toplevel`
 release=`git describe --abbrev=0 --tags`
@@ -10,5 +15,12 @@ new_release="$version.$mayor.$new_minor"
 echo "package utils" > $PWD/utils/version.go
 echo "" >> $PWD/utils/version.go
 echo "const VERSION = \"$new_release\"" >> $PWD/utils/version.go
+echo "+ git add $PWD/utils/version.go"
 git add $PWD/utils/version.go
-git tag $new_release
+echo "+ git commit -m \"Release $new_release\""
+git commit -m \"Release $new_release\"
+echo "+ git tag $new_release `git rev-parse HEAD`"
+git tag $new_release `git rev-parse HEAD`
+echo "+ git push"
+git push
+gorelease
--- utils/version.go
+++ utils/version.go
@@ -1,3 +1,3 @@
 package utils
 
-const VERSION = "0.4.2"
+const VERSION = "0.4.3"
