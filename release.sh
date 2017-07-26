072bed1ec0de71caceaa23a5e8d33c0001d6efe9
Javi Perez-Griffo <javier@besol.es>
2017-07-26 15:42:54 +0200

    Release 0.4.4

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ fi
 go get github.com/goreleaser/goreleaser
 git fetch --tags
 pwd=`git rev-parse --show-toplevel`
-release=`git describe --abbrev=0 --tags`
+release=`git tag | tail -n 1`
 version=`echo $release | awk -F \. '{ print $1 }'`
 mayor=`echo $release | awk -F \. '{ print $2 }'`
 minor=`echo $release | awk -F \. '{ print $3 }'`
@@ -18,7 +18,7 @@ echo "const VERSION = \"$new_release\"" >> $PWD/utils/version.go
 echo "+ git add $PWD/utils/version.go"
 git add $PWD/utils/version.go
 echo "+ git commit -m \"Release $new_release\""
-git commit -m \"Release $new_release\"
+git commit -m "Release $new_release"
 echo "+ git tag $new_release `git rev-parse HEAD`"
 git tag $new_release `git rev-parse HEAD`
 echo "+ git push"
--- utils/version.go
+++ utils/version.go
@@ -1,3 +1,3 @@
 package utils
 
-const VERSION = "0.4.3"
+const VERSION = "0.4.4"
