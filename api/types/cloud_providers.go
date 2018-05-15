17d6aa2169f36bb1d6d5b18b407e3fc41a110879
Pablo Cantera <p.cantera@sofcloudit.com>
2018-05-15 13:00:37 +0200

    Remove the CLI property "ProvidedServices" as a property for a Cloud Provider (issue #46)
    Since IMCO API does not provide values anymore; remove the CLI property "ProvidedServices" as a property for a Cloud Provider.
    
    Closes #46

--- api/types/cloud_providers.go
+++ api/types/cloud_providers.go
@@ -4,5 +4,4 @@ type CloudProvider struct {
 	Id                  string   `json:"id" header:"ID"`
 	Name                string   `json:"name" header:"NAME"`
 	RequiredCredentials []string `json:"required_credentials" header:"REQUIRED_CREDENTIALS"`
-	ProvidedServices    []string `json:"provided_services" header:"PROVIDED_SERVICES"`
 }
--- testdata/cloud_providers_data.go
+++ testdata/cloud_providers_data.go
@@ -9,13 +9,11 @@ func GetCloudProviderData() *[]types.CloudProvider {
 		{
 			Id:                  "fakeID0",
 			Name:                "fakeName0",
-			ProvidedServices:    []string{"fakeService01", "fakeService02", "fakeService03"},
 			RequiredCredentials: []string{"fakeCredential01", "fakeCredential02"},
 		},
 		{
 			Id:                  "fakeID1",
 			Name:                "fakeName1",
-			ProvidedServices:    []string{"fakeService11", "fakeService12"},
 			RequiredCredentials: []string{"fakeCredential11", "fakeCredential12", "fakeCredential13"},
 		},
 	}
