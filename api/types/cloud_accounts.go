dc1ff35ba5bd904e53cccac4c55b506d87afd8e6
Pablo Cantera <p.cantera@sofcloudit.com>
2018-06-05 10:47:03 +0200

    Added more detailed report for Cloud Accounts (issue #63)
    Closes #63

--- api/types/cloud_accounts.go
+++ api/types/cloud_accounts.go
@@ -1,8 +1,10 @@
 package types
 
 type CloudAccount struct {
-	Id          string `json:"id" header:"ID"`
-	CloudProvId string `json:"cloud_provider_id" header:"CLOUD_PROVIDER_ID"`
+	Id            string `json:"id" header:"ID"`
+	Name          string `json:"name" header:"NAME"`
+	CloudProvId   string `json:"cloud_provider_id" header:"CLOUD_PROVIDER_ID"`
+	CloudProvName string `json:"cloud_provider_name" header:"CLOUD_PROVIDER_NAME"`
 }
 
 type RequiredCredentials interface{}
