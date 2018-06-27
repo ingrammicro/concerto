e6a69e9185d0db82958d5b4915eac6b26c63b488
Pablo Cantera <p.cantera@sofcloudit.com>
2018-06-27 15:01:19 +0200

    Added documented description to avoid exiting during tests (issue #76)

--- utils/format/formatter.go
+++ utils/format/formatter.go
@@ -7,6 +7,9 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+// Required workaround for testing os.Exit(1) scenarios in Go with coverage.
+// Otherwise, PrintFatal cannot be evaluated due to os.Exit() cannot be captured.
+// Implemented in test files (json/text)
 var osExit = os.Exit
 
 // Formatter defines output printing interface
