95d0754b2ff93ef41e9cdd622ea0c727895e62d1
Pablo Cantera <pablo.cantera@ingrammicro.com>
2019-04-11 20:53:15 +0200

    Added firewall test(issue #89)
    Closes #89

---
+++ api/firewall/firewall_api_test.go
@@ -0,0 +1,40 @@
+package firewall
+
+import (
+	"github.com/ingrammicro/concerto/testdata"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewDispatcherServiceNil(t *testing.T) {
+	assert := assert.New(t)
+	rs, err := NewFirewallService(nil)
+	assert.Nil(rs, "Uninitialized service should return nil")
+	assert.NotNil(err, "Uninitialized service should return error")
+}
+
+func TestGetPolicy(t *testing.T) {
+	pIn := testdata.GetPolicyData()
+	GetPolicyMocked(t, pIn)
+	GetPolicyFailErrMocked(t, pIn)
+	GetPolicyFailStatusMocked(t, pIn)
+	GetPolicyFailJSONMocked(t, pIn)
+}
+
+func TestAddPolicyRule(t *testing.T) {
+	pIn := testdata.GetPolicyData()
+	for _, pr := range pIn.Rules {
+		AddPolicyRuleMocked(t, &pr)
+		AddPolicyRuleFailErrMocked(t, &pr)
+		AddPolicyRuleFailStatusMocked(t, &pr)
+		AddPolicyRuleFailJSONMocked(t, &pr)
+	}
+}
+
+func TestUpdateServer(t *testing.T) {
+	pIn := testdata.GetPolicyData()
+	UpdatePolicyMocked(t, pIn)
+	UpdatePolicyFailErrMocked(t, pIn)
+	UpdatePolicyFailStatusMocked(t, pIn)
+	UpdatePolicyFailJSONMocked(t, pIn)
+}
---
+++ testdata/firewall_data.go
@@ -0,0 +1,29 @@
+package testdata
+
+import (
+	"github.com/ingrammicro/concerto/api/types"
+)
+
+// GetPolicyData loads test data
+func GetPolicyData() *types.Policy {
+	testPolicy := types.Policy{
+		ActualRules: []types.PolicyRule{
+			{
+				Protocol: "fakeProtocol0",
+				MinPort:  0,
+				MaxPort:  1024,
+				Cidr:     "fakeCidrIP0",
+			},
+		},
+		Rules: []types.PolicyRule{
+			{
+				Protocol: "fakeProtocol1",
+				MinPort:  0,
+				MaxPort:  1024,
+				Cidr:     "fakeCidrIP1",
+			},
+		},
+	}
+
+	return &testPolicy
+}
